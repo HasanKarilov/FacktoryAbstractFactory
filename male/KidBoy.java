084735d36d7d350c082d9f7ad2c9a6bbc765cc01
HasanKarilov <hkarilov@gmail.com>
2017-04-06 11:13:31 +0600

    Factory Pattern

---
+++ male/KidBoy.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task37.task3702.male;
+
+import com.javarush.task.task37.task3702.Human;
+
+/**
+ * Created by hanaria on 4/5/17.
+ */
+public class KidBoy implements Human
+{
+    public static final int MAX_AGE = 12;
+
+    @Override
+    public String toString()
+    {
+        return "KidBoy{}";
+    }
+}
---
+++ male/TeenBoy.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task37.task3702.male;
+
+import com.javarush.task.task37.task3702.Human;
+
+/**
+ * Created by hanaria on 4/5/17.
+ */
+public class TeenBoy implements Human
+{
+    public static final int MAX_AGE = 19;
+
+    @Override
+    public String toString()
+    {
+        return "TeenBoy{}";
+    }
+}
